223f14a5dcdc9713781a574855de2458ed6cae52
JojoGraff <johannes.graff2001@gmail.com>
2021-04-16 10:32:37 +0200

    changed to List connections

--- src/main/scala/de/htwg/se/ScotlandYard/model/Cell.scala
+++ src/main/scala/de/htwg/se/ScotlandYard/model/Cell.scala
@@ -1,6 +1,6 @@
 package de.htwg.se.ScotlandYard.model
 
-case class Cell(value:Int, name:Int, connection: Int) {
+case class Cell(value:Int, name:Int, connection: List[Int]) {
   def isTaxi : Boolean = value == 1
   def isBus : Boolean = value == 2
   def isSub : Boolean = value == 3
@@ -10,5 +10,5 @@ case class Cell(value:Int, name:Int, connection: Int) {
    * @return name
    */
   def getName : Int = name
-
+  def getConnections : List[Int] = connection
 }
--- src/test/scala/de/htwg/se/ScotlandYard/model/CellSpec.scala
+++ src/test/scala/de/htwg/se/ScotlandYard/model/CellSpec.scala
@@ -7,28 +7,40 @@ import org.scalatest.wordspec.AnyWordSpec
 class CellSpec extends AnyWordSpec with Matchers {
   "A Cell" when {
     "not set to any value" should {
-      val emptyCell = Cell(0,0,0)
+      val emptyCell = Cell(0,0,Nil)
       "have value 0" in {
-        emptyCell.value == 0 && emptyCell.name == 0 && emptyCell.connection == 0
+        emptyCell.value == 0 && emptyCell.name == 0 && emptyCell.connection == Nil
       }
       "not be set to anything" in {
         emptyCell.isSub should be (false)
         emptyCell.isTaxi should be (false)
         emptyCell.isBus should be (false)
+        emptyCell.name should be (0)
+        emptyCell.getConnections should be (Nil)
       }
     }
     "set to taxi and given a name" should {
-      val taxiCell = Cell(1,13,0)
+      val taxiCell = Cell(1,13,List(1,2))
       "return its status" in {
         taxiCell.value should be (1)
+        taxiCell.connection should be (List(1,2))
       }
-      "have value 1 and have a name and have 0 connection " in {
+      "have value 1 and have a name and have a connection" in {
         taxiCell.value should be(1)
         taxiCell.name should be (13)
-        taxiCell.connection should be (0)
+        taxiCell.connection should be (List(1,2))
+      }
+      "return something on getConnection" in {
+        taxiCell.getConnections should be (List(1,2))
       }
       "be set to taxi" in {
         taxiCell.isTaxi should be (true)
+      }
+      "set to any name" should {
+        val nameCell = Cell(1,123,Nil)
+        "return its name" in {
+          nameCell.getName should be (123)
+        }
       }
     }
   }
