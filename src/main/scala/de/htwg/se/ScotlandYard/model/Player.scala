c34ebb33d7f0e70868c40a722f238ace8d8aca94
tobias-mack <tobias.mack_gg@yahoo.de>
2021-04-16 10:55:14 +0200

    Player and PlayerSpec updated + added some comments

--- src/main/scala/de/htwg/se/ScotlandYard/model/Player.scala
+++ src/main/scala/de/htwg/se/ScotlandYard/model/Player.scala
@@ -1,10 +1,23 @@
 package de.htwg.se.ScotlandYard.model
-
+/*
+* status also represents the order in which the players make a move
+*
+*/
 case class Player(name: String, status: Int) {
    override def toString:String = name
-   def mrX : Boolean = status == 0
-   def hunter: Boolean = status != 0
 
+
+   /**
+    * returns boolean if player is Mr.X
+    * @return Boolean
+    */
+   def isMrX : Boolean = status == 0
+
+   /**
+    * returns boolean if player is Hunter
+    * @return Boolean
+    */
+   def isHunter: Boolean = status != 0
 
 }
 
--- src/test/scala/de/htwg/se/ScotlandYard/model/PlayerSpec.scala
+++ src/test/scala/de/htwg/se/ScotlandYard/model/PlayerSpec.scala
@@ -10,12 +10,12 @@ class PlayerSpec extends AnyWordSpec with Matchers {
       val player1 = Player("Player1",1)
       "return its status, not be Mr.X, but be Hunter" in {
         player1.status should be (1)
-        player1.status != 0
-        player1.mrX should be (false)
-        player1.hunter should be(true)
+        player1.isMrX should be (false)
+        player1.isHunter should be(true)
       }
       "have name Player1 " in {
         player1.name should be("Player1")
+        player1.toString should be ("Player1")
       }
     }
   }
