1876eaf22cfd2dbcd7a1d087d463a7ccc4fbb041
JojoGraff <johannes.graff2001@gmail.com>
2021-04-23 00:10:26 +0200

    added tui, not yet implemented

---
+++ src/main/scala/de/htwg/se/ScotlandYard/aview/Tui.scala
@@ -0,0 +1,14 @@
+/*
+package de.htwg.se.ScotlandYard.aview
+import de.htwg.se.ScotlandYard.model.{Cell,Player,Board}
+
+class Tui {
+
+  def processInputLine(input: String, board: Board): Board = {
+    input match {
+      case "moveTo" =>
+    }
+  }
+
+}*/
+
