780b2cd8cac2dcf7cd44d0250af3684071bb72e0
JojoGraff <johannes.graff2001@gmail.com>
2021-04-23 00:07:53 +0200

    updated to very simple board structure with 3 cells

--- src/main/scala/de/htwg/se/ScotlandYard/model/Board.scala
+++ src/main/scala/de/htwg/se/ScotlandYard/model/Board.scala
@@ -1,13 +1,7 @@
 package de.htwg.se.ScotlandYard.model
 
-case class Board() {
-
-  val cell1: Cell = Cell(1,1,List(2),0)
-  val cell2: Cell = Cell(2,2,List(3),0)
-  val cell3: Cell = Cell(3,3,List(1),0)
-
-  def getCells: (Int,Int,Int) = {
-    (cell1.getName, cell2.getName, cell3.getName)
-  }
-
+case class Board(cell1: Cell,cell2: Cell,cell3: Cell) {
+  def getCell1: Cell = cell1
+  def getCell2: Cell = cell2
+  def getCell3: Cell = cell3
 }
--- src/test/scala/de/htwg/se/ScotlandYard/model/BoardSpec.scala
+++ src/test/scala/de/htwg/se/ScotlandYard/model/BoardSpec.scala
@@ -7,22 +7,19 @@ import org.scalatest.wordspec.AnyWordSpec
 class BoardSpec extends AnyWordSpec with Matchers {
   "A Board" when {
     "when created" should {
-      val board1 = new Board
-        "have 3 cells" in {
-          board1.getCells should be (1,2,3)
-        }
-        "have connections to each other" in {
-          board1.cell1.connection should be (List(2))
-          board1.cell2.connection should be (List(3))
-          board1.cell3.connection should be (List(1))
-        }
-        "be a taxi/bus/sub" in {
-          board1.cell1.isTaxi should be (true)
-          board1.cell2.isBus should be (true)
-          board1.cell3.isSub should be (true)
+      val c = Cell(1,1,Nil,0)
+      val cc = Cell(1,2,Nil,0)
+      val ccc = Cell(1,3,Nil,1)
+      val board1 = Board(c,cc,ccc)
+      "retun its cells" in {
+        board1.getCell1 should be (c)
+        board1.getCell2 should be (cc)
+        board1.getCell3 should be (ccc)
+
+      }
+
         }
     }
-  }
 
 }
 
