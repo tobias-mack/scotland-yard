a8aa81113f723b4cf5c439c4c2c5def9909535a4
JojoGraff <johannes.graff2001@gmail.com>
2021-04-22 17:38:59 +0200

    remade player

--- src/main/scala/de/htwg/se/ScotlandYard/model/Player.scala
+++ src/main/scala/de/htwg/se/ScotlandYard/model/Player.scala
@@ -1,11 +1,21 @@
 package de.htwg.se.ScotlandYard.model
+
+import scala.collection.mutable
+
 /*
 * status also represents the order in which the players make a move
 *
 */
-case class Player(name: String, status: Int) {
-   override def toString:String = name
-   def isMrX : Boolean = status == 0
-   def isHunter: Boolean = status != 0
+case class Player(name: String, tickets: (Int,Int,Int), position: Int) {
+
+   def showTicketsLeft(): (Int,Int,Int) = tickets
+   def getPosition: Int = position
+   def getName: String = name
+
+   def moveTo(field: Int): Player = {
+      val ret = Player(getName,showTicketsLeft(),field)
+      ret
+   }
+
 }
 
