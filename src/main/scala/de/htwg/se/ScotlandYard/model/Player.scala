2f4bc5056d30f10346a10cd02a3b8200bec9fc15
JojoGraff <johannes.graff2001@gmail.com>
2021-04-22 17:47:36 +0200

    remade player

--- src/main/scala/de/htwg/se/ScotlandYard/model/Player.scala
+++ src/main/scala/de/htwg/se/ScotlandYard/model/Player.scala
@@ -1,6 +1,5 @@
 package de.htwg.se.ScotlandYard.model
 
-import scala.collection.mutable
 
 /*
 * status also represents the order in which the players make a move
@@ -13,7 +12,8 @@ case class Player(name: String, tickets: (Int,Int,Int), position: Int) {
    def getName: String = name
 
    def moveTo(field: Int): Player = {
-      val ret = Player(getName,showTicketsLeft(),field)
+      val newTickets = (showTicketsLeft()._1 -1, showTicketsLeft()._2, showTicketsLeft()._3)
+      val ret = Player(getName,newTickets,field)
       ret
    }
 
