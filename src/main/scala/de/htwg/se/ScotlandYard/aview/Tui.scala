bfb52b38a03046c86c6d1857b12817bcbe91774f
JojoGraff <johannes.graff2001@gmail.com>
2021-04-23 10:57:04 +0200

    updated player

--- src/main/scala/de/htwg/se/ScotlandYard/aview/Tui.scala
+++ src/main/scala/de/htwg/se/ScotlandYard/aview/Tui.scala
@@ -9,9 +9,9 @@ class Tui {
 
   def processInputLine(input: String, board: Board): Player = {
     input match {
-      case "moveTo1" => board.player1.moveTo(board.cell1,1)
-      case "moveTo2" => board.player1.moveTo(board.cell2,2)
-      case "moveTo3" => board.player1.moveTo(board.cell3,3)
+      case "moveTo1" => board.player1.setCell(board.player1,board.cell1)
+      case "moveTo2" => board.player1.setCell(board.player1,board.cell2)
+      case "moveTo3" => board.player1.setCell(board.player1,board.cell3)
     }
   }
 
