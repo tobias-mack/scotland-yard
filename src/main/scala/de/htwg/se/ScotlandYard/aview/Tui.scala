33c2d71bca15de69a2b441dd45e663536449d133
tobias-mack <tobias.mack_gg@yahoo.de>
2021-05-13 15:38:32 +0200

    merged GameStart in Tui + updated input match

--- src/main/scala/de/htwg/se/ScotlandYard/aview/Tui.scala
+++ src/main/scala/de/htwg/se/ScotlandYard/aview/Tui.scala
@@ -8,21 +8,39 @@ import scala.io.StdIn.readLine
 class Tui (controller: Controller) extends Observer{
   controller.add(this)
 
+  def gameStart(): Unit = {
+    val playerNumber: Int = readLine("""||==== Welcome to Scotland-Yard! ====|
+                                        ||   How many players want to play?  |
+                                        ||   Type a number between 2-5: """.stripMargin).toInt
+    val detectives: Unit = Vector.tabulate(playerNumber) {
+      n => controller.addDetective(
+        if(n==0) {readLine(s"Mister X, type your name: ")}
+        else {readLine(s"Player ${n + 1}, type your name: ")})
+    }
+    println(":LET THE GAME BEGIN!")
+    println("Mr.X starts...type in: [means of transport]")
+  }
+
   def processInputLine(input: String, order: Int): Unit = {
+    val taxi = """(t|T)(a|A)(x|X)(i|I)"""
+    val bus = "(b|B)(u|U)(s|S)"
+    val subway = "(s|S)(u|U)(b|B)"
+    val black = "(b|B)(l|L)(a|A)(c|C)(k|K)"
     input match {
-      case "Taxi" => println("where to?")
+      case s if s.matches(taxi) => println("where to?")
                       val pos = readLine().toInt
                       controller.movePlayer(pos,order,1)
-      case "Bus" => println("where to?")
+      case s if s.matches(bus) => println("where to?")
                       val pos = readLine().toInt
                       controller.movePlayer(pos,order,2)
-      case "Sub" => println("where to?")
+      case s if s.matches(subway) => println("where to?")
                       val pos = readLine().toInt
                       controller.movePlayer(pos,order,3)
-      case "black" => println("where to?")
+      case s if s.matches(black) => println("where to?")
                       val pos = readLine().toInt
                       controller.movePlayer(pos,order,4)
       case _ =>
+        System.exit(0)
         scala.io.StdIn.readLine("command does not exist. Try again.\n")
     }
   }
