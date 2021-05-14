9a1dd4161011e89f3607aa7344d73bbca71b1fad
tobias-mack <tobias.mack_gg@yahoo.de>
2021-05-14 11:48:36 +0200

    added tests for strategies

--- src/test/scala/de/htwg/se/ScotlandYard/model/BoardHardStrategySpec.scala
+++ src/test/scala/de/htwg/se/ScotlandYard/model/BoardHardStrategySpec.scala
@@ -1,9 +1,10 @@
 package de.htwg.se.ScotlandYard.model
+
 import org.scalatest.matchers.should.Matchers
 import org.scalatest.wordspec.AnyWordSpec
 
-class BoardDefaultStrategySpec extends AnyWordSpec with Matchers {
-    "A Default Board" when {
+class BoardHardStrategySpec extends AnyWordSpec with Matchers {
+    "A Board on Hard Mode" when {
       val board1 = Board()
       val board2 = Board().addDetective(Board(),"jo")
       val board3 = board2.addDetective(board2,"xo")
@@ -20,30 +21,29 @@ class BoardDefaultStrategySpec extends AnyWordSpec with Matchers {
         }
       }
       "should be able to move around" should {
-        val board4 = (new BoardDefaultStrategy).movePlayer(board3,15,0,1)
-        val board5 = (new BoardDefaultStrategy).movePlayer(board4,3,1,2)
-        val board6 = (new BoardDefaultStrategy).movePlayer(board5,12,0,3)
-        val board7 = (new BoardDefaultStrategy).movePlayer(board6,11,1,4)
+        val board4 = (new BoardHardStrategy).movePlayer(board3,15,0,1)
+        val board5 = (new BoardHardStrategy).movePlayer(board4,3,1,2)
+        val board6 = (new BoardHardStrategy).movePlayer(board5,12,0,3)
+        val board7 = (new BoardHardStrategy).movePlayer(board6,11,1,4)
         "in " in {
-          board4.player(0) should be (MisterX("jo",Cell(15),Ticket(8,5,3,5)))
-          board5.player(1) should be (Detective("xo", Cell(3), Ticket(10,7,4)))
-          //board5.player(0) should be (MisterX("jo",Cell(15),Ticket(8,5,3,5)))
-          board6.player(0) should be (MisterX("jo",Cell(12),Ticket(8,5,2,5)))
-          board7.player(1) should be (Detective("xo",Cell(11),Ticket(10,7,4,-1)))
+          board4.player(0) should be (MisterX("jo",Cell(15),Ticket(7,5,3,5)))
+          board5.player(1) should be (Detective("xo", Cell(3), Ticket(10,6,4)))
+          board6.player(0) should be (MisterX("jo",Cell(12),Ticket(7,5,1,5)))
+          board7.player(1) should be (Detective("xo",Cell(11),Ticket(10,6,4,-1)))
         }
         " and also print the output" in {
           board5.toString should be ("  \u001b[30m" + "jo" + " \u001b[0mis at \u001b[34mposition " + "15" + " \u001b[0mand has\u001b[33m " +
-            "8 Taxi \u001b[0mtickets,\u001b[32m" + "5 Bus \u001b[0mtickets, \u001b[31m" +
+            "7 Taxi \u001b[0mtickets,\u001b[32m" + "5 Bus \u001b[0mtickets, \u001b[31m" +
             "3 Subway \u001b[0mtickets" + "; \n" +
             "  \u001b[30m" + "xo" + " \u001b[0mis at \u001b[34mposition " + "3" + " \u001b[0mand has\u001b[33m " +
-            "10 Taxi \u001b[0mtickets,\u001b[32m" + "7 Bus \u001b[0mtickets, \u001b[31m" +
+            "10 Taxi \u001b[0mtickets,\u001b[32m" + "6 Bus \u001b[0mtickets, \u001b[31m" +
             "4 Subway \u001b[0mtickets" + "; \n" +
             "  Board: 1 , 2 , 3 ")
           board7.toString should be ("  \u001b[30m" + "jo" + " \u001b[0mis at \u001b[34mposition " + "12" + " \u001b[0mand has\u001b[33m " +
-            "8 Taxi \u001b[0mtickets,\u001b[32m" + "5 Bus \u001b[0mtickets, \u001b[31m" +
-            "2 Subway \u001b[0mtickets" + "; \n" +
+            "7 Taxi \u001b[0mtickets,\u001b[32m" + "5 Bus \u001b[0mtickets, \u001b[31m" +
+            "1 Subway \u001b[0mtickets" + "; \n" +
             "  \u001b[30m" + "xo" + " \u001b[0mis at \u001b[34mposition " + "11" + " \u001b[0mand has\u001b[33m " +
-            "10 Taxi \u001b[0mtickets,\u001b[32m" + "7 Bus \u001b[0mtickets, \u001b[31m" +
+            "10 Taxi \u001b[0mtickets,\u001b[32m" + "6 Bus \u001b[0mtickets, \u001b[31m" +
             "4 Subway \u001b[0mtickets" + "; \n" +
             "  Board: 1 , 2 , 3 ")
         }
