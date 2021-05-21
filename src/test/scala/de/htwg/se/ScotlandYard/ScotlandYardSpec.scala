1407d2aaf7a4f857d781219701b11d4769c9d1b4
JojoGraff <johannes.graff2001@gmail.com>
2021-05-21 09:43:19 +0200

    added test for main

---
+++ src/test/scala/de/htwg/se/ScotlandYard/ScotlandYardSpec.scala
@@ -0,0 +1,27 @@
+package de.htwg.se.ScotlandYard
+import de.htwg.se.ScotlandYard.aview.Tui
+import de.htwg.se.ScotlandYard.controller.Controller
+import de.htwg.se.ScotlandYard.model.Board
+import org.scalatest.matchers.should.Matchers
+import org.scalatest.wordspec.AnyWordSpec
+
+import java.io.ByteArrayInputStream
+
+class ScotlandYardSpec extends AnyWordSpec with Matchers {
+
+  "ScotlandYard" when {
+    val controller1 = new Controller(Board())
+    val tui = new Tui(controller1)
+    "take input" in {
+      val in = new ByteArrayInputStream("q".getBytes())
+      Console.withIn(in) {ScotlandYard.main(Array("2","taxi","q"))}
+    }
+
+    "quit" should {
+      "quit" in {
+        val in = new ByteArrayInputStream("q".getBytes)
+        Console.withIn(in) {ScotlandYard.main(Array())}
+      }
+    }
+  }
+}
