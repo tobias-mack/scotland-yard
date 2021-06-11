cbeaff28431da50fde1d0f36854eb1953749e000
JojoGraff <johannes.graff2001@gmail.com>
2021-06-11 10:38:13 +0200

    added all Stations

--- src/main/scala/de/htwg/se/ScotlandYard/aview/StationLocater.scala
+++ src/main/scala/de/htwg/se/ScotlandYard/aview/StationLocater.scala
@@ -6,7 +6,26 @@ import scala.collection.immutable.{HashMap, HashSet}
 
 object StationLocater {
   val hashMap = HashMap("1"->Point(-70,-440),
-                        "2"->Point(370,-50)
+                        "2"->Point(370,-50),
+                        "3"->Point(430,-40),
+                        "4"->Point(410,100),
+                        "5"->Point(330,100),
+                        "6"->Point(210,220),
+                        "7"->Point(140,170),
+                        "8"->Point(165,0),
+                        "9"->Point(-310,-160),
+                        "10"->Point(-390,-250),
+                        "11"->Point(-630,-320),
+                        "12"->Point(-580,-140),
+                        "13"->Point(-380,-160),
+                        "14"->Point(-480,-80),
+                        "15"->Point(-100,170),
+                        "16"->Point(100,280),
+                        "17"->Point(-100,350),
+                        "18"->Point(-15,380),
+                        "19"->Point(100,350),
+                        "20"->Point(400,-440),
+                        "21"->Point(150,-180)
   )
   def findXYpos(goToLoc: String): Option[Point] ={
     hashMap.get(goToLoc)
