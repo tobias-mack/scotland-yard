88da26fe9b78f8eefc4f872a27028373e388af85
JojoGraff <johannes.graff2001@gmail.com>
2021-06-24 17:22:58 +0200

    cell.toString

--- src/main/scala/de/htwg/se/ScotlandYard/model/gameComponents/Cell.scala
+++ src/main/scala/de/htwg/se/ScotlandYard/model/gameComponents/Cell.scala
@@ -1,8 +1,15 @@
 package de.htwg.se.ScotlandYard.model.gameComponents
 
+
 case class Cell (number: Int = 0,
                 nearbyTaxi: List[Int] = List(),
                 nearbyBus: List[Int] = List(),
                 nearbySub: List[Int] = List()) {
+
+  @Override
+  override def toString: String = {
+    val s = " " + number.toString + " " + nearbyTaxi.toString() + " " + nearbyBus.toString() + " " + nearbySub.toString()
+    s
+  }
 
 }
