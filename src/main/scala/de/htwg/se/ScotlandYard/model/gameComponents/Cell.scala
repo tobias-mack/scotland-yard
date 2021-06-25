5914d8a6a66a92eff23b31a284a040edc9926ad1
tobias-mack <tobias.mack_gg@yahoo.de>
2021-06-25 14:59:44 +0200

    cell updated

--- src/main/scala/de/htwg/se/ScotlandYard/model/gameComponents/Cell.scala
+++ src/main/scala/de/htwg/se/ScotlandYard/model/gameComponents/Cell.scala
@@ -1,17 +1,3 @@
 package de.htwg.se.ScotlandYard.model.gameComponents
 
-
-case class Cell (number: Int = 0) {
-
-  @Override
-  override def toString: String = {
-    number.toString
-  }
-
-  object Cell {
-    import play.api.libs.json._
-    implicit val cellWrites = Json.writes[Cell]
-    implicit val cellReads = Json.reads[Cell]
-  }
-
-}
+case class Cell (number: Int = 0) {}
