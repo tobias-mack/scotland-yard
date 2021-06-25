ddc1770b49e3f2e4e5c7e99a8cd997e94215f0f4
JojoGraff <johannes.graff2001@gmail.com>
2021-06-25 11:35:03 +0200

    updated tests

--- src/test/scala/de/htwg/se/ScotlandYard/model/BoardSpec.scala
+++ src/test/scala/de/htwg/se/ScotlandYard/model/BoardSpec.scala
@@ -1,4 +1,3 @@
-/*
 
 
 package de.htwg.se.ScotlandYard.model
@@ -10,8 +9,8 @@ import org.scalatest.wordspec.AnyWordSpec
 class BoardSpec extends AnyWordSpec with Matchers {
   "A Board" when {
     val board1 = Board()
-    val board2 = Board().addDetective(Board(),"jo")
-    val board3 = board2.addDetective(board2,"xo")
+    val board2 = Board().addDetective(Board(),"jo",Cell(5),Ticket(9,5,3,5))
+    val board3 = board2.addDetective(board2,"xo",Cell(1),Ticket(10,8,4))
     "created" should {
       "should have starting parameters" in {
         board1.player should be(Vector[Player]())
@@ -42,5 +41,5 @@ class BoardSpec extends AnyWordSpec with Matchers {
   }
 }
 
-*/
+
 
