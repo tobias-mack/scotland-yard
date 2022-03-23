35ebf78d00dcba5d1dec17cf3849bae77431099b
JojoGraff <johannes.graff2001@gmail.com>
2022-03-23 15:53:50 +0100

    SA01: small correction

--- src/main/scala/de/htwg/se/ScotlandYard/util/State.scala
+++ src/main/scala/de/htwg/se/ScotlandYard/util/State.scala
@@ -3,5 +3,5 @@ package de.htwg.se.ScotlandYard.util
 import de.htwg.se.ScotlandYard.controller.controllerBaseImpl.GameState
 
 trait State[T] {
-  def handle(string: String,state: GameState): Void
+  def handle(string: String,state: GameState): Unit
 }
