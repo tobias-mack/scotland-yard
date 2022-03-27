9cf61582b4d35eecba92aea9f322e3a7fbded0f5
tobias-mack <tobias.mack_gg@yahoo.de>
2022-03-27 17:30:05 +0200

    SA01: fixed injection error

--- src/main/scala/de/htwg/se/ScotlandYard/ScotlandYardModule.scala
+++ src/main/scala/de/htwg/se/ScotlandYard/ScotlandYardModule.scala
@@ -1,6 +1,6 @@
 package de.htwg.se.ScotlandYard
 
-import com.google.inject.AbstractModule
+import com.google.inject.{AbstractModule, Provides, TypeLiteral}
 import com.google.inject.name.Names
 import de.htwg.se.ScotlandYard.controller.ControllerInterface
 import net.codingwell.scalaguice.ScalaModule
@@ -17,7 +17,7 @@ class ScotlandYardModule extends AbstractModule {
   val default: Vector[Player] = Vector[Player]()
 
   override def configure():Unit = {
-    bind(classOf[Vector[Player]]).annotatedWith(Names.named("DefaultPlayer")).toInstance(default)
+    bind(new TypeLiteral[Vector[Player]]() {}).annotatedWith(Names.named("DefaultPlayer")).toInstance(default)
     bind(classOf[ControllerInterface]).to(classOf[controllerBaseImpl.Controller])
     bind(classOf[BoardInterface]).to(classOf[gameComponents.Board])
     bind(classOf[FileIOInterface]).to(classOf[fileIO_JSON_Impl.FileIOJSON]) //fileIO_XML_Impl.FileIOXML
