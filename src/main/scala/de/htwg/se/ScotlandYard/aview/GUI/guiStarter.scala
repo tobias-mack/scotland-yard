09f5a8dbfbd3aace02f8597b5e9e54f7059e6bea
tobias-mack <tobias.mack_gg@yahoo.de>
2022-03-29 22:18:26 +0200

    SA01: fixed bug: javaFx Toolkit not initialised

--- src/main/scala/de/htwg/se/ScotlandYard/aview/GUI/guiStarter.scala
+++ src/main/scala/de/htwg/se/ScotlandYard/aview/GUI/guiStarter.scala
@@ -3,11 +3,12 @@ package de.htwg.se.ScotlandYard.aview.GUI
 import de.htwg.se.ScotlandYard.aview
 import de.htwg.se.ScotlandYard.aview.Tui
 import de.htwg.se.ScotlandYard.controller.ControllerInterface
+import javafx.embed.swing.JFXPanel
 
 object guiStarter {
   def apply(ui: String, controller: ControllerInterface): Unit = {
     ui match {
-      case "gui" => aview.GUI.GUI(controller).run()
+      case "gui" => new JFXPanel(); aview.GUI.GUI(controller).run()
       case "tui" =>
         val tui = Tui(controller)
         println("Welcome to Scotland Yard:\n" +
