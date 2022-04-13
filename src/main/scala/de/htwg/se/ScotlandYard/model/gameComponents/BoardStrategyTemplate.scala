0947954bd2c5dd4eb9785e50977d3f1107195c53
tobias-mack <tobias.mack_gg@yahoo.de>
2022-04-14 00:46:36 +0200

    SA04: refactored updatePlayer method

--- src/main/scala/de/htwg/se/ScotlandYard/model/gameComponents/BoardStrategyTemplate.scala
+++ src/main/scala/de/htwg/se/ScotlandYard/model/gameComponents/BoardStrategyTemplate.scala
@@ -5,29 +5,16 @@ import de.htwg.se.ScotlandYard.model.BoardInterface
 trait BoardStrategyTemplate:
 
   def movePlayer(board: BoardInterface, pos: Int, playerNumber: Int, transport: Int): Board =
-    val player1 = Some(board.player(playerNumber))
+    val player = board.player(playerNumber)
+    updatePlayer(board, playerNumber, Some(newData(player, pos, transport)))
+
+  def newData(player: Player, pos: Int, transport: Int): Player =
+    val t = player.ticket
     transport match
-      case 1 => updatePlayer(board, playerNumber, Some(newDataTaxi(player1, pos)))
-      case 2 => updatePlayer(board, playerNumber, Some(newDataBus(player1, pos)))
-      case 3 => updatePlayer(board, playerNumber, Some(newDataSubway(player1, pos)))
-      case 4 => updatePlayer(board, playerNumber, Some(newDataBlack(player1, pos)))
-
-
-  def newDataTaxi(player: Option[Player], pos: Int): Player =
-    player.get.setCell(player.get, Cell(pos), Ticket(player.get.ticket.taxi - ticketUsage(), player.get.ticket.bus,
-      player.get.ticket.subway, player.get.ticket.black))
-
-  def newDataBus(player: Option[Player], pos: Int): Player =
-    player.get.setCell(player.get, Cell(pos), Ticket(player.get.ticket.taxi, player.get.ticket.bus - ticketUsage(),
-      player.get.ticket.subway, player.get.ticket.black))
-
-  def newDataSubway(player: Option[Player], pos: Int): Player =
-    player.get.setCell(player.get, Cell(pos), Ticket(player.get.ticket.taxi, player.get.ticket.bus,
-      player.get.ticket.subway - ticketUsage(), player.get.ticket.black))
-
-  def newDataBlack(player: Option[Player], pos: Int): Player =
-    player.get.setCell(player.get, Cell(pos), Ticket(player.get.ticket.taxi, player.get.ticket.bus,
-      player.get.ticket.subway, player.get.ticket.black - ticketUsage()))
+      case 1 => player.setCell(player, Cell(pos), Ticket(t.taxi - ticketUsage(), t.bus, t.subway, t.black))
+      case 2 => player.setCell(player, Cell(pos), Ticket(t.taxi, t.bus - ticketUsage(), t.subway, t.black))
+      case 3 => player.setCell(player, Cell(pos), Ticket(t.taxi, t.bus, t.subway - ticketUsage(), t.black))
+      case 4 => player.setCell(player, Cell(pos), Ticket(t.taxi, t.bus, t.subway, t.black - ticketUsage()))
 
   def ticketUsage(): Int
 
