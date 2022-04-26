d2544b56c6cd58fc67afa5d69f038e1d590ac8c2
JojoGraff <johannes.graff2001@gmail.com>
2022-04-26 22:51:08 +0200

    added akka dependencies

--- project/dependencies.scala
+++ project/dependencies.scala
@@ -15,7 +15,19 @@ object dependencies {
 	val scalaGuice = ("net.codingwell" %% "scala-guice" % "5.0.1").cross(CrossVersion.for3Use2_13)
 	val scalaXml = "org.scala-lang.modules" %% "scala-xml" % "2.0.1"
 	val playJson = "com.typesafe.play" %% "play-json" % "2.10.0-RC6"
+	val akkaHttp = ("com.typesafe.akka" %% "akka-http" % versionNumber.akkaHttpVersion).cross(CrossVersion.for3Use2_13)
+	val akkaHttpSpray = ("com.typesafe.akka" %% "akka-http-spray-json" % versionNumber.akkaHttpVersion).cross(CrossVersion.for3Use2_13)
+	val akkaHttpCore = ("com.typesafe.akka" %% "akka-http-core" % versionNumber.akkaHttpVersion).cross(CrossVersion.for3Use2_13)
+	val akkaActorTyped = ("com.typesafe.akka" %% "akka-actor-typed" % versionNumber.akka).cross(CrossVersion.for3Use2_13)
+	val akkaStream = ("com.typesafe.akka" %% "akka-stream" % versionNumber.akka).cross(CrossVersion.for3Use2_13)
+	val akkaActor = ("com.typesafe.akka" %% "akka-actor" % versionNumber.akka).cross(CrossVersion.for3Use2_13)
+	val slf4jNop = "org.slf4j" % "slf4j-nop" % versionNumber.slf4jNop
 
+	object versionNumber {
+		val akka = "2.6.19"
+		val akkaHttpVersion = "10.2.9"
+		val slf4jNop = "2.0.0-alpha7"
+	}
 	//scalacOptions
 	val scalacDeprecation = "-deprecation"
 	val scalacFeature = "-feature"
@@ -24,7 +36,8 @@ object dependencies {
 	// Projects
 	val commonDependencies =
 		Seq(scalactic, scalatest, apacheCommons, apacheCommonsIO, scalaGraph,	scalaFX,
-			googleGuice,	scalaGuice,	scalaXml, playJson)
+			googleGuice,	scalaGuice,	scalaXml, playJson, akkaHttp, akkaHttpSpray, akkaHttpCore,
+			akkaActorTyped, akkaStream, akkaActor, slf4jNop)
 
 	val commonScalacOptions = Seq(scalacDeprecation, scalacFeature, scalacreflectiveCalls)
 }
