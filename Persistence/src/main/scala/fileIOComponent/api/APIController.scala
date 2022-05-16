9d2d258bd08899966d0d2c67680b781ac49a60d7
JojoGraff <johannes.graff2001@gmail.com>
2022-05-16 23:18:26 +0200

    SA07: Start with Slick

--- Persistence/src/main/scala/fileIOComponent/api/APIController.scala
+++ Persistence/src/main/scala/fileIOComponent/api/APIController.scala
@@ -1,22 +1,30 @@
 package fileIOComponent.api
 
-import com.google.inject.{Guice, Inject}
-
-import java.io._
+import com.google.inject.{Guice, Inject, Injector}
+import fileIOComponent.{FileIOInterface, PersistenceModule, FileIOInterface }
+import fileIOComponent.databaseComponent.{DBInterface,DAOInterface}
+import java.io.*
 import play.api.libs.json.{JsValue, Json}
+
 import scala.io.Source
 
-object APIController {
+object APIController :
 
-  def load(): String = {
+  val injector: Injector = Guice.createInjector(PersistenceModule)
+  val fileIO = injector.getInstance(classOf[FileIOInterface])
+  val database = injector.getInstance(classOf[DBInterface])
+  val databaseDAO = injector.getInstance(classOf[DAOInterface])
+
+
+  def load(): String =
     val file = scala.io.Source.fromFile("board.json")
     try file.mkString finally file.close()
-  }
 
-  def save(gameAsJson: String): Unit = {
+
+  def save(gameAsJson: String): Unit =
     val pw = new PrintWriter(new File("." + File.separator + "game.json"))
     pw.write(gameAsJson)
     pw.close
-  }
 
-}
+
+
