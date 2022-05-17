79a1bbb21ba0d7ebf7c854808ee4682cf7e7a91e
tobias-mack <tobias.mack_gg@yahoo.de>
2022-05-17 22:55:29 +0200

    SA07: initializeDB with players

--- Persistence/src/main/scala/fileIOComponent/databaseComponent/Slick/DAOSlickImpl.scala
+++ Persistence/src/main/scala/fileIOComponent/databaseComponent/Slick/DAOSlickImpl.scala
@@ -16,54 +16,70 @@ import scala.io.StdIn
 import scala.util.{Failure, Success, Try}
 
 
-class DAOSlickImpl  @Inject ()extends DAOInterface:
+class DAOSlickImpl @Inject() extends DAOInterface :
 
-  val connectIP = sys.env.getOrElse("POSTGRES_IP", "localhost").toString
-  val connectPort = sys.env.getOrElse("POSTGRES_PORT", 5432).toString.toInt
-  val database_user = sys.env.getOrElse("POSTGRES_USER", "postgres").toString
-  val database_pw = sys.env.getOrElse("POSTGRES_PASSWORD", "postgres").toString
-  val database_name = sys.env.getOrElse("POSTGRES_DB", "postgres").toString
+	val connectIP = sys.env.getOrElse("POSTGRES_IP", "localhost").toString
+	val connectPort = sys.env.getOrElse("POSTGRES_PORT", 5432).toString.toInt
+	val database_user = sys.env.getOrElse("POSTGRES_USER", "postgres").toString
+	val database_pw = sys.env.getOrElse("POSTGRES_PASSWORD", "postgres").toString
+	val database_name = sys.env.getOrElse("POSTGRES_DB", "postgres").toString
 
-  val database =
-    Database.forURL(
-      url = s"jdbc:postgresql://$connectIP:$connectPort/${database_name}?serverTimezone=UTC",
-      user = database_user,
-      password = database_pw,
-      driver = "org.postgresql.Driver")
+	val database =
+		Database.forURL(
+			url = s"jdbc:postgresql://$connectIP:$connectPort/${database_name}?serverTimezone=UTC",
+			user = database_user,
+			password = database_pw,
+			driver = "org.postgresql.Driver")
 
-  val playerTable = TableQuery[PlayerTable]
-  val gameTable = TableQuery[GameTable]
+	val playerTable = TableQuery[PlayerTable]
+	val gameTable = TableQuery[GameTable]
 
-  override def create: Unit =
-    val gameDB = Future(Await.result(database.run(DBIO.seq(
-      playerTable.schema.createIfNotExists,
-      gameTable.schema.createIfNotExists
-    )), Duration.Inf))
-    gameDB.onComplete {
-      case Success(_) => print("Connection to DB & Creation of Tables successful!")
-      case Failure(e) => print("Error: " + e)
-    }
-
-
-  override def read: String =
-    val query = sql"""SELECT * FROM PLAYER""".as[(Int, String, Int, Int, Int, Int, Int, Int)] // not sure
-    val result = Await.result(database.run(query), atMost = 10.second)
-    result.toString()
+	override def create: Unit =
+		val gameDB = Future(Await.result(database.run(
+			DBIO.seq(
+				playerTable.schema.createIfNotExists,
+				gameTable.schema.createIfNotExists
+			)), Duration.Inf))
+		gameDB.onComplete {
+			case Success(_) =>
+				print("Connection to DB & Creation of Tables successful!")
+				//initializeDatabase
+			case Failure(e) => print("\nError: " + e + "\n")
+		}
 
 
-  override def update(input: String): Unit =
-    val gameJson: JsValue = Json.parse(input)
-    val currentPlayer = (gameJson \ "gameInformation" \ "currentPlayer").get.toString().toInt
-    val travelLog = (gameJson \ "gameInformation" \ "travelLog").get.toString()
-    val revealCounter = (gameJson \ "gameInformation" \ "revealCounter").get.toString().toInt
-    val playerNumber = (gameJson \ "gameInformation" \ "playerNumber").get.toString().toInt
+	def initializeDatabase =
+		val initializer = Future(Await.result(database.run(
+			DBIO.seq(
+				playerTable ++= Seq(
+					(0, "mrX", 2, 5, 5, 5, 5, 1),
+					(1, "detective", 20, 5, 5, 5, 5, 0)
+				)
+			)), Duration.Inf))
+		initializer.onComplete {
+			case Success(_) =>
+				print("\ninitialized Database!")
+			case Failure(e) => print("\nError: " + e + "\n")
+		}
 
 
+	override def read: String =
+		val query = sql"""SELECT * FROM PLAYER""".as[(Int, String, Int, Int, Int, Int, Int, Int)] // not sure
+		val result = Await.result(database.run(query), atMost = 10.second)
+		result.toString()
 
 
-  //weird
-  override def delete: Unit =
-    val action = playerTable.delete
-    Await.result(database.run(action), atMost = 10.second)
-    val deleteQuery = sql"""ALTER SEQUENCE "PLAYER_id_seq" RESTART WITH 1""".as[Int]
-    Await.result(database.run(deleteQuery), atMost = 10.second)
+	override def update(input: String): Unit =
+		val gameJson: JsValue = Json.parse(input)
+		val currentPlayer = (gameJson \ "gameInformation" \ "currentPlayer").get.toString().toInt
+		val travelLog = (gameJson \ "gameInformation" \ "travelLog").get.toString()
+		val revealCounter = (gameJson \ "gameInformation" \ "revealCounter").get.toString().toInt
+		val playerNumber = (gameJson \ "gameInformation" \ "playerNumber").get.toString().toInt
+
+
+	//weird
+	override def delete: Unit =
+		val action = playerTable.delete
+		Await.result(database.run(action), atMost = 10.second)
+		val deleteQuery = sql"""ALTER SEQUENCE "PLAYER_id_seq" RESTART WITH 1""".as[Int]
+		Await.result(database.run(deleteQuery), atMost = 10.second)
